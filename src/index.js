bdc5f059acc022a1533ccc2c840bc4d1967a121c
arupriyamondal <arupriyamondal2004@gmail.com>
2026-03-27 23:15:37 +0530

    change the cors local api

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://desun-contest-kc-am.netlify.app/",
   credentials: true,
 }));
 
