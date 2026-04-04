e582936f935f124f9d1d3f8190f7c8627b4e73e4
arupriyamondal <arupriyamondal2004@gmail.com>
2026-04-04 13:22:33 +0530

    removing the admin from for get all particiate

--- src/routers/participants.routes.js
+++ src/routers/participants.routes.js
@@ -2,11 +2,11 @@ import { Router } from "express";
 
 import { verifyJWT } from "../middlewares/verifyJwt.js";
 import { addParticipant, getAllParticipants, submitProject } from "../controllers/participants.controller.js";
-import { isAdmin } from "../middlewares/isAdmin.js";
+
 const participantrouter = Router();
 
 participantrouter.post("/join", verifyJWT, addParticipant);
 participantrouter.put("/submit", verifyJWT, submitProject);
-participantrouter.get("/all",verifyJWT,isAdmin, getAllParticipants);
+participantrouter.get("/all",verifyJWT, getAllParticipants);
 
 export default participantrouter;
